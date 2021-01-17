9f3edd5a7635ee976a4af0eed3f604ec8760174e
noel <noel_emmanuel@pm.me>
2021-01-17 16:49:24 +1000

    converting project to bulk download

--- README.md
+++ README.md
@@ -1,20 +1,15 @@
 # youtube-to-mp3
-High quality YouTube video -> audio converter in CLI. Built using Python3. Embeds artist name and cover image into songs.
-
-![app demo](https://i.ibb.co/frtwyXD/v3.gif)
-
+Python3 script to bulk download youtube links in .txt file to mp3 format. Embeds artist name and cover image into songs.
 
 ## Setup & Installation
-### video demo
-https://vimeo.com/479255729
 1. Clone this repo (either by SSH-ing or by direct downloading it)
 2. Navigate to `youtube-to-mp3` folder which you have downloaded
 3. [Create and activate  virtual environment for Python3](https://docs.python.org/3/library/venv.html) I named mine ytenv
 4. The requirements are listed in `requirements.txt` You can install these easily by `pip install -r requirements.txt`
 5. Install PyTube separately through `python -m pip install git+https://github.com/nficano/pytube` 
+6. Populate `links.txt` with the youtube video URLs line by line.
 6. Run `python app.py` 
 
 
 ## Notes
-- The artist name field is required for the album art to be embedded properly
-- PyTube has been observed to NOT work for a long period of time due to changes in YouTube's internal structure. Feel free to open any issues :)
+- PyTube is always being updated and newer versions of this software are being released due to changes happening in YouTube's internal structure. Feel free to open any issues :)
--- app.py
+++ app.py
@@ -10,79 +10,83 @@ import requests
 # user inputs
 
 
-print("""
- __     _________       __     __  __ _____ ____  
- \ \   / /__   __|      \ \   |  \/  |  __ \___ \ 
-  \ \_/ /   | |     _____\ \  | \  / | |__) |__) |
-   \   /    | |    |______> > | |\/| |  ___/|__ < 
-    | |     | |          / /  | |  | | |    ___) |
-    |_|     |_|         /_/   |_|  |_|_|   |____/ 
-                                                  
-                                                  
-""")
 
-yt_link = input("Enter The YouTube Link { with https } : \n")
 path1 = '.'
-artist_name = input("Enter artist_name : \n")
 
-# downloading video using pytube
-yt = YouTube(yt_link)
-# now we have a YouTube object called yt
-# printing the title of the video to terminal screen
-song_name = yt.title
-print(song_name)
-img_id = yt.thumbnail_url
-# see available media formats
-yt.streams.all()
-# getting the first stream option
-stream = yt.streams.first()
-# downloading video to path2
-path2 = stream.download(path1)
-mp4_file = path2
-# creating a path for mp3 file
-path3 = path1 + "/" +  song_name + ".mp3"
-mp3_file = path3
+# Open the file with read only permit
+g = open('links.txt')
+# use readline() to read the first line 
+line = g.readline()
+# use the read line to read further.
+# If the file is not empty keep reading one line
+# at a time, till the file is empty
+while line:
+	print(line)
 
-# conversion from mp4 to mp3
-videoclip = VideoFileClip(mp4_file) # create a videoclip object
-audioclip = videoclip.audio # get the audio object from mp4 file
-audioclip.write_audiofile(mp3_file) # save the audio of mp4 into an mp3 file
-audioclip.close()
-videoclip.close()
+	# downloading video using pytube
+	yt = YouTube(line)
+	artist_name = yt.streams[0].title
+	# now we have a YouTube object called yt
+	# printing the title of the video to terminal screen
+	song_name = yt.title
+	print(song_name)
+	img_id = yt.thumbnail_url
 
-os.remove(path2)
+	# see available media formats
+	yt.streams.all()
+	# getting the first stream option
+	stream = yt.streams.first()
+	# downloading video to path2
+	path2 = stream.download(path1)
+	mp4_file = path2
+	# creating a path for mp3 file
+	path3 = path1 + "/" +  song_name + ".mp3"
+	mp3_file = path3
 
-#--------- DOWNLOADING YOUTUBE THUMBNAIL IMAGE -----------------
+	# conversion from mp4 to mp3
+	videoclip = VideoFileClip(mp4_file) # create a videoclip object
+	audioclip = videoclip.audio # get the audio object from mp4 file
+	audioclip.write_audiofile(mp3_file) # save the audio of mp4 into an mp3 file
+	audioclip.close()
+	videoclip.close()
 
-# message confirming that the img download process has started
-print('Importing youtube thumbnail...')
+	os.remove(path2)
 
-url = img_id
-r = requests.get(url)
+	#--------- DOWNLOADING YOUTUBE THUMBNAIL IMAGE -----------------
 
-# assigning a path for the temporary image file
-# (it's going to get deleted after embed onto mp3)
-path4 = path1 + "/img" + ".jpg"
+	# message confirming that the img download process has started
+	print('Importing youtube thumbnail...')
 
-with open(path4, 'wb') as f:
-    f.write(r.content)
+	url = img_id
+	r = requests.get(url)
 
-# retrieving HTTP meta-data
-print(r.status_code)
-print(r.headers['content-type'])
-print(r.encoding)
+	# assigning a path for the temporary image file
+	# (it's going to get deleted after embed onto mp3)
+	path4 = path1 + "/img" + ".jpg"
 
-#--------- EMBEDDING THUMBNAIL IMG ONTO MP3 FILE -----------------
+	with open(path4, 'wb') as f:
+	    f.write(r.content)
 
-audiofile = eyed3.load(mp3_file)
-if (audiofile.tag == None):
-    audiofile.initTag()
+	# retrieving HTTP meta-data
+	print(r.status_code)
+	print(r.headers['content-type'])
+	print(r.encoding)
 
-audiofile.tag.images.set(3, open(path4,'rb').read(), 'image/jpeg')
-audiofile.tag.album_artist = artist_name
-audiofile.tag.save()
+	#--------- EMBEDDING THUMBNAIL IMG ONTO MP3 FILE -----------------
 
-# deleting the thumbnail as it's not needed anymore
-os.remove(path4)
-print(song_name + " downloaded successfully :)")
+	audiofile = eyed3.load(mp3_file)
+	if (audiofile.tag == None):
+	    audiofile.initTag()
+
+	audiofile.tag.images.set(3, open(path4,'rb').read(), 'image/jpeg')
+	audiofile.tag.album_artist = artist_name
+	audiofile.tag.save()
+
+	# deleting the thumbnail as it's not needed anymore
+	os.remove(path4)
+	print(song_name + " downloaded successfully :)")
+	# reading next line
+	line = g.readline()
+
+f.close()
 
